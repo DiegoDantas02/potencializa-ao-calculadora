5838ca716aba64a882651bc63559c7d4181d17b2
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-04-13 22:25:11 -0300

    Add project files.

---
+++ potencializaçao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a base: ");
+        double baseNum = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o expoente: ");
+        int expoente = int.Parse(Console.ReadLine());
+
+        double resultado = Math.Pow(baseNum, expoente);
+
+        Console.WriteLine("O resultado de " + baseNum + " elevado a " + expoente + " é igual a " + resultado);
+
+        Console.ReadLine();
+    }
+}
